5ebaccb09a3650a25c18799f93127d3ae6fe0e7f
Deysha Rivera <50204868+rivtechprojects@users.noreply.github.com>
2025-05-16 16:36:29 -0400

    swap to ngx-monaco-editor and add assets to build, reinstate jasmine for now, move snippet logic to snippet component (#13)
    * swap to ngx-monaco-editor and add assets to build, reinstate jasmine for now, move snippet logic to snippet component
    
    * temporarily add legacy peer deps until angular 20 release
    
    * update for angular convention

--- clipvault-frontend/src/app/components/snippet/snippet.ts
+++ clipvault-frontend/src/app/components/snippet/snippet.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { NgStyle, CommonModule } from '@angular/common';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { languageMock } from '../../../Mocks/language.mock';
+import { NGX_MONACO_EDITOR_CONFIG, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 @Component({
   selector: 'app-snippet',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    NgStyle, 
+    MatChipsModule, 
+    MatIconModule, 
+    MonacoEditorModule, 
+    MatSelectModule, 
+    FormsModule],
   templateUrl: './snippet.html',
-  styleUrl: './snippet.scss'
+  styleUrls: ['./snippet.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    {
+      provide: NGX_MONACO_EDITOR_CONFIG,
+      useValue: {
+        baseUrl: 'assets'
+      } as NgxMonacoEditorConfig
+    }
+  ]
 })
 export class Snippet {
+  @Input() snippet: any;
+  @Input() expanded = false;
+  @Output() expand = new EventEmitter<void>();
+  @Output() close = new EventEmitter<void>();
 
+  languages = languageMock;
+
+  onExpand() {
+    this.expand.emit();
+  }
+  onClose() {
+    this.close.emit();
+  }
 }
