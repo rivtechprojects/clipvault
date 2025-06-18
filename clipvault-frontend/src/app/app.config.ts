fa57be465cd3fe5b16e3f4cd78b6f4d78978f5cd
RivTech <50204868+RivTechProjects@users.noreply.github.com>
2025-06-18 00:10:03 -0400

    unused import

--- clipvault-frontend/src/app/app.config.ts
+++ clipvault-frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
